3806522887f1cd135892e6129e29b61a4ee092de
Mohit76655 <mohit76655@gmail.com>
2024-06-24 14:17:57 +0000

    v 1.1

--- aws_resource_tracker.sh
+++ aws_resource_tracker.sh
@@ -4,34 +4,43 @@
 # Author: Mohit
 # Date: 18th-Jan
 #
-#  Version: v1
+# Version: v1.1
 #
-#  This script will report the aws resource
-#
+# This script reports AWS resources.
 ########################
 
-set -x
+set -e
 
-# AWS S3
-# AWS EC2
-# AWS Lambda
-# AWS IAM User
+# Function to list S3 buckets
+list_s3_buckets() {
+    echo "Listing S3 buckets..."
+    aws s3 ls
+}
 
-# list s3 buckets
-echo "Print list of s3 buckets"
-aws s3 ls
+# Function to list EC2 instances
+list_ec2_instances() {
+    echo "Listing EC2 instances..."
+    aws ec2 describe-instances --query 'Reservations[*].Instances[*].InstanceId' --output text
+}
 
-# list Ec2 Instance
-echo "print list of EC2 Instances"
-aws ec2 describe-instances | jq '.Reservations[].Instances[].InstanceId'
+# Function to list Lambda functions
+list_lambda_functions() {
+    echo "Listing Lambda functions..."
+    aws lambda list-functions --query 'Functions[*].FunctionName' --output text
+}
 
-# list lambda
-echo "Print list of lambda functions"
-aws lambda list-functions
+# Function to list IAM users
+list_iam_users() {
+    echo "Listing IAM users..."
+    aws iam list-users --query 'Users[*].UserName' --output text
+}
 
-# list IAM User
-echo "Print list of IAM User"
-aws iam list-users
+# Execute functions
+list_s3_buckets
+list_ec2_instances
+list_lambda_functions
+list_iam_users
+
 
 
 
