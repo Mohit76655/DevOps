961b891802cd5844960d7558e4205fecae775d94
Mohit76655 <mohit76655@gmail.com>
2024-07-07 10:32:00 +0000

    updates (v1.1)

--- Projects/Backup.sh
+++ Projects/Backup.sh
@@ -9,6 +9,7 @@ readMe
 source_dir=$1
 target_dir=$2
 
+function create_backup {
 timestamp=$(date '+%Y-%m-%d-%H-%M-%S')
 
 backup_dir="${target_dir}/backup_${timestamp}"
@@ -20,4 +21,19 @@ if [ $? -eq 0 ]; then
 else
     echo "Backup was not performed for $timestamp"
 fi
+}
+
+function perform_rotation {
+    local backups=($(ls -t "${target_dir}/backup_"*.zip))
+	
+    if [ "${#backups[@]}" -gt 3 ]; then
+        local backups_to_remove=("${backups[@]:3}")
+        for backup in "${backups_to_remove[@]}"; do
+            rm -f "$backup"
+        done
+    fi
+}
+
+create_backup
+perform_rotation
 
